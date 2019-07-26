6c3db3fd064ac965a8d5942a0d87b1ef72bc291b
EdwinHoenselaar <EdwinHoenselaar@gmail.com>
2019-07-26 11:39:30 +0200

    http request to the db with words

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -1,10 +1,30 @@
 <template>
-  <h1>Test</h1>
+  <div>
+    <h1>{{this.currentWord.word}}</h1>
+    <h3>{{this.currentWord.description}}</h3>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Word',
+    data() {
+    return {
+      words:[],
+      currentWord: {},
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/words')
+      .then(res => {
+        this.words = res.data.word
+        this.currentWord = this.words[Math.floor(Math.random() * this.words.length)]
+      })
+      .catch(err => console.log(err))
+  },
 }
 </script>
 
